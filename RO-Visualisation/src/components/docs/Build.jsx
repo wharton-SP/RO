5a8ab2f26a1f7978402fc575a35b5bf1be08a232
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-05-31 11:05:46 +0300

    Update : enhance Build component header with icon and improve keyboard shortcut formatting

--- RO-Visualisation/src/components/docs/Build.jsx
+++ RO-Visualisation/src/components/docs/Build.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import { Bolt } from 'lucide-react'
 
 const Build = () => {
     return (
         <section id="construction">
-            <h2>🧱 1. Construction du Graphe</h2>
+            <h2 className='flex items-center gap-3'><Bolt /> <span className='text-lg font-bold'>Construction du Graphe</span></h2>
             <ul>
                 <li>👉 Cliquez n’importe où dans la zone de dessin pour <strong>ajouter un nœud</strong>.</li>
                 <li>
                     👉 Pour <strong>ajouter une arête</strong> :
-                    <kbd className="kbd">Shift</kbd> + clic sur le nœud de départ, puis
-                    <kbd className="kbd">Shift</kbd> + clic sur le nœud d’arrivée.
+                    <kbd className="kbd">Shift</kbd> + <kbd className="kbd">Clic</kbd> sur le nœud de départ, puis
+                    <kbd className="kbd">Shift</kbd> + <kbd className="kbd">Clic</kbd> sur le nœud d’arrivée.
                 </li>
                 <li>Le nœud sélectionné est entouré d’un cercle jaune.</li>
                 <li>Vous pouvez déplacer les nœuds à tout moment, même s’ils ne sont pas sélectionnés.</li>
