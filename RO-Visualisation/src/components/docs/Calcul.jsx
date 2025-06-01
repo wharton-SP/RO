c86f01ceaaaa4c73fa5053ffac91d2a5182ab58b
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-06-01 08:45:41 +0300

    Update : enhance layout and content clarity in Calcul component

--- RO-Visualisation/src/components/docs/Calcul.jsx
+++ RO-Visualisation/src/components/docs/Calcul.jsx
@@ -1,17 +1,25 @@
+import { Workflow } from 'lucide-react'
 import React from 'react'
 
 const Calcul = () => {
     return (
         <section id="calcul">
-            <h2>🚀 4. Calcul du Flot Max</h2>
-            <p>
-                Une fois le graphe prêt, cliquez sur le bouton
-                <button className="btn btn-accent btn-sm mx-2">Flot Max</button>
-                pour lancer l’algorithme.
-            </p>
-            <p>
-                Les étapes du calcul seront affichées de façon progressive : chemins augmentants, mise à jour des flots, etc.
-            </p>
+            <h2 className='flex items-center gap-3'><Workflow /> <span className='text-4xl font-bold'>Calcul du Flot Maximal.</span></h2>
+            <div className='pt-2'>
+                <p className='italic badge badge-soft badge-warning'>Assurez-vous que votre graph est prêt.</p>
+            </div>
+            <div>
+                <p className='font-bold menu-title text-lg'>Lancer le calcul</p>
+                <p className='px-5 pb-2'>
+                    - Appuye juste sur <button className="btn btn-sm btn-secondary"><Workflow /> Flot Max</button>
+                </p>
+                <p>
+                    Les étapes du calcul seront affichées de façon progressive : chemins augmentants, mise à jour des flots, etc.
+                </p>
+            </div>
+            <div className='pt-2'>
+                <p className='italic badge badge-soft badge-info'>Le graph sera envoyer vers le backend où il sera traité. Il sera ensuite retourné avec les etapes de résolution.</p>
+            </div>
         </section>
     )
 }
