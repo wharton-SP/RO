dd2fbdf591664ff253ddc4999f9a43a0edb26fb7
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-06-01 09:24:53 +0300

    Update : restructure SimpleNav component for improved layout and organization

--- RO-Visualisation/src/components/UI/SimpleNav.jsx
+++ RO-Visualisation/src/components/UI/SimpleNav.jsx
@@ -6,7 +6,15 @@ const SimpleNav = ({ handleThemeChange }) => {
     return (
         <div className="navbar bg-base-100 shadow-sm flex justify-between">
             <Link to="/" className="text-xl">RO | Flot Max</Link>
-            <ThemeChanger handleThemeChange={handleThemeChange} />
+            <div className='flex items-center gap-3'>
+                <div className='flex gap-5'>
+                    <Link to="/" className='hover:underline hover:text-primary transition-all'>Accueil</Link>
+                    <Link to="/home" className='hover:underline hover:text-primary transition-all'>Graph</Link>
+                    <Link to="/docs" className='hover:underline hover:text-primary transition-all'>Docs</Link>
+                </div>
+                <div className="divider divider-horizontal"></div>
+                <ThemeChanger handleThemeChange={handleThemeChange} />
+            </div>
         </div>
     )
 }
