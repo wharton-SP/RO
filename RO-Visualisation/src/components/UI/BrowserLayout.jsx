07f0f32d4b9e996c0ab6b83b2f0e5014305cb326
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-06-01 10:17:09 +0300

    Update : refactor BrowserLayout component to improve structure and remove unnecessary elements

--- RO-Visualisation/src/components/UI/BrowserLayout.jsx
+++ RO-Visualisation/src/components/UI/BrowserLayout.jsx
@@ -5,10 +5,7 @@ import ThumbGraph from '../ThumbGraph'
 
 const BrowserLayout = ({theme}) => {
     return (
-        <div className="mockup-browser border border-base-300 h-full w-full">
-            <div className="mockup-browser-toolbar">
-                <div className="input">https://RO-xxxxxx/xxxx</div>
-            </div>
+        <div className="mockup-window border border-base-300 h-full w-full">
             <motion.div layoutId='graphBox' className='flex justify-center items-center h-full'>
                 <ThumbGraph theme={theme}/>
             </motion.div>
