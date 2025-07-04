cc01fef83769268e82776e3b41ab9fdef811bb2b
Wharton Aldrick (Sanctuary) <whartonaldrick@gmail.com>
2025-07-04 11:31:09 +0300

    Update : add Electron integration with main process setup and preload script

---
+++ RO-Visualisation/electron/electron.cjs
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+    });
+
+    // Charger ton build React ou localhost en dev
+    if (process.env.NODE_ENV === 'development') {
+        win.loadURL('http://localhost:5173');
+    } else {
+        win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
