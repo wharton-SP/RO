d61fcbf9afc1f7e4672d504d09c1152868a374ef
Xeon <whartonaldrick@gmail.com>
2026-06-18 12:18:16 +0300

    Fix: correct typo in API base URL in sendRequest utility

--- RO-Visualisation/src/utils/sendRequest.js
+++ RO-Visualisation/src/utils/sendRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://ro-back.whartonaldrick.com/api/";
+const API_BASE_URL = "https://ro-back.wharton-aldrick.com/api/";
 
 const sendRequest = async (url, method, data) => {
     try {
